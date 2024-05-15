94411a3a6d4a5d9d7e686d7a0c9a83ce36eefa5e
nicola02nb <61830443+nicola02nb@users.noreply.github.com>
2024-05-15 13:02:24 +0200

    Update AutoSwitchStatus.plugin.js

--- AutoSwitchStatus.plugin.js
+++ AutoSwitchStatus.plugin.js
@@ -46,6 +46,11 @@ const config = {
         github_raw: "https://raw.githubusercontent.com/nicola02nb/AutoSwitchStatus/main/AutoSwitchStatus.plugin.js"
     },
     changelog: [{
+        title: "0.5.1",
+            items: [
+                "Added Translations"
+            ]
+        },{
         title: "0.5.0",
             items: [
                 "Removed startupStatus setting and functionality",
@@ -203,6 +208,8 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
         Webpack,
         Webpack: { Filters },
     } = BdApi;
+    
+    const LanguageStore = Webpack.getModule(Webpack.Filters.byProps("getLocale"));
 
     const UserSettingsProtoStore = BdApi.Webpack.getModule(
         (m) =>
@@ -243,6 +250,8 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
         onStart() {
             log_debug("<<PluginSettings>>\n"+JSON.stringify(this.settings, null, 4));
             
+            this.languageCode=this.getClientLanguage();
+            this.languageTranslation=this.getStatusTranslations(this.languageCode);
             this.status=this.settings.statuses.disconnectedStatus;
             this.updateTime=this.settings.updateTime;
             this.updateStatus(this.status);
@@ -264,6 +273,7 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
             var toSet=this.getUserCurrentStatus();
 
             log_debug("<<Variables Status>>" +
+                        "\nLanguage: " + this.languageCode +
                         "\nChannelId: " + this.channelId +
                         "\nUpdateTime: " + this.updateTime + "ms" +
                         "\nStatus: " + this.status +
@@ -287,7 +297,7 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
 
         /**
          * Funtion that returns the current user status
-         * @returns {('online'|'idle'|'invisible'|'dnd')} toStatus
+         * @returns {('online'|'idle'|'invisible'|'dnd')}
          * @throws when the mute buttons aren't found
          */
         getUserCurrentStatus(){
@@ -346,7 +356,11 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
          * @param {('online'|'idle'|'invisible'|'dnd')} toStatus
          */
         updateStatus(toStatus) {
-            console.log(toStatus);
+            if(this.languageCode!=this.getClientLanguage()){
+                this.languageCode=this.getClientLanguage();
+                this.languageTranslation=this.getStatusTranslations(this.languageCode);
+            }
+
             if (DEBUG_ActuallyChangeStatus) {
                 log_debug("Changing (but not changing) status to: " + toStatus);
                 return;
@@ -355,12 +369,11 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
             UserSettingsProtoUtils.updateAsync(
                 "status",
                 (statusSetting) => {
-                    //log_debug(statusSetting);
                     statusSetting.status.value = toStatus;
-                    this.showToast(toStatus);
                 },
                 0
             );
+            this.showToast(this.languageTranslation[toStatus]);
         }
 
         /**
@@ -372,6 +385,205 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
                 BdApi.showToast(msg);
             }
         }
+
+        getClientLanguage(){
+            const currentLocale = LanguageStore.getLocale();
+            return currentLocale.split("-")[0];    
+        }
+
+        getStatusTranslations(languageCode) {
+            switch (languageCode) {
+                case "bg": // Bulgarian
+                    return {
+                        online: "Онлайн",
+                        idle: "Неактивен",
+                        invisible: "Невидим",
+                        dnd: "Не се притеснявайте"
+                    };
+                case "da": // Danish
+                    return {
+                        online: "Online",
+                        idle: "Inaktiv",
+                        invisible: "Usynlig",
+                        dnd: "Optaget"
+                    };
+                case "de": // German
+                    return {
+                        online: "Online",
+                        idle: "Inaktiv",
+                        invisible: "Unsichtbar",
+                        dnd: "Bitte nicht stören"
+                    };
+                case "el": // Greek
+                    return {
+                        online: "Συνδεδεμένος",
+                        idle: "Αδρανής",
+                        invisible: "Αόρατος",
+                        dnd: "Μην ενοχλείτε"
+                    };
+                case "es": // Spanish
+                    return {
+                        online: "En línea",
+                        idle: "Inactivo",
+                        invisible: "Invisible",
+                        dnd: "No molestar"
+                    };
+                case "fi": // Finnish
+                    return {
+                        online: "Paikalla",
+                        idle: "Poissa",
+                        invisible: "Näkymätön",
+                        dnd: "Älä häiritse"
+                    };
+                case "fr": // French
+                    return {
+                        online: "En ligne",
+                        idle: "Inactif",
+                        invisible: "Invisible",
+                        dnd: "Ne pas déranger"
+                    };
+                case "hr": // Croatian
+                    return {
+                        online: "Dostupan",
+                        idle: "Neaktivan",
+                        invisible: "Nevidljiv",
+                        dnd: "Ne uznemiravaj"
+                    };
+                case "hu": // Hungarian
+                    return {
+                        online: "Elérhető",
+                        idle: "Tétlen",
+                        invisible: "Láthatatlan",
+                        dnd: "Ne zavarjanak"
+                    };
+                case "it": // Italian
+                    return {
+                        online: "Online",
+                        idle: "Inattivo",
+                        invisible: "Invisibile",
+                        dnd: "Non disturbare"
+                    };
+                case "ja": // Japanese
+                    return {
+                        online: "オンライン",
+                        idle: "アイドル",
+                        invisible: "不在",
+                        dnd: "取り込み中"
+                    };
+                case "ko": // Korean
+                    return {
+                        online: "온라인",
+                        idle: "비활성",
+                        invisible: "숨김",
+                        dnd: "다른 용무 중"
+                    };
+                case "lt": // Lithuanian
+                    return {
+                        online: "Prisijungęs",
+                        idle: "Neaktyvus",
+                        invisible: "Nematomas",
+                        dnd: "Ne(trukdyti)"
+                    };
+                case "nl": // Dutch
+                    return {
+                        online: "Online",
+                        idle: "Inactief",
+                        invisible: "Onzichtbaar",
+                        dnd: "Niet storen"
+                    };
+                case "no": // Norwegian
+                    return {
+                        online: "Tilgjengelig",
+                        idle: "Ikke tilgjengelig",
+                        invisible: "Usynlig",
+                        dnd: "Ikke forstyrr"
+                    };
+                case "pl": // Polish
+                    return {
+                        online: "Dostępny",
+                        idle: "Nieobecny",
+                        invisible: "Niewidoczny",
+                        dnd: "Nie przeszkadzać"
+                    };
+                case "pt-BR": // Portuguese (Brazil)
+                    return {
+                        online: "Online",
+                        idle: "Ausente",
+                        invisible: "Invisível",
+                        dnd: "Não perturbe"
+                    };
+                case "ro": // Romanian
+                    return {
+                        online: "Conectat",
+                        idle: "Absent",
+                        invisible: "Invizibil",
+                        dnd: "Nu deranjați"
+                    };
+                case "ru": // Russian
+                    return {
+                        online: "Онлайн",
+                        idle: "Неактивен",
+                        invisible: "Невидимый",
+                        dnd: "Не беспокоить"
+                    };
+                case "sv": // Swedish
+                    return {
+                        online: "Tillgänglig",
+                        idle: "Inaktiv",
+                        invisible: "Osynlig",
+                        dnd: "Stör ej"
+                    };
+                case "th": // Thai
+                    return {
+                        online: "ออนไลน์",
+                        idle: "ไม่อยู่",
+                        invisible: "ซ่อน",
+                        dnd: "ไม่สะดวก"
+                    };
+                case "tr": // Turkish
+                    return {
+                        online: "Çevrimiçi",
+                        idle: "Uyku modu",
+                        invisible: "Görünmez",
+                        dnd: "Rahatsız Etmeyin"
+                    };
+                case "uk": // Ukrainian
+                    return {
+                        online: "Онлайн",
+                        idle: "Неактивний",
+                        invisible: "Невидимий",
+                        dnd: "Не турбувати"
+                    };
+                case "vi": // Vietnamese
+                    return {
+                        online: "Trực tuyến",
+                        idle: "Vắng mặt",
+                        invisible: "Ẩn danh",
+                        dnd: "Đừng làm phiền"
+                    };
+                case "zh-CN": // Chinese (China)
+                    return {
+                        online: "在线",
+                        idle: "离开",
+                        invisible: "隐身",
+                        dnd: "请勿打扰"
+                    };
+                case "zh-TW": // Chinese (Taiwan)
+                    return {
+                        online: "線上",
+                        idle: "離開",
+                        invisible: "隱身",
+                        dnd: "請勿打擾"
+                    };
+                default: // Default (English)
+                    return {
+                        online: "Online",
+                        idle: "Idle",
+                        invisible: "Invisible",
+                        dnd: "Do Not Disturb"
+                    };
+            }
+        }        
     };
 };
      return plugin(Plugin, Api);
