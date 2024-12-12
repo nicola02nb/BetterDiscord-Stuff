1b01161d65d1ffd059c2f7a1320237b3c3219ffe
Nicola <61830443+nicola02nb@users.noreply.github.com>
2024-12-12 11:05:24 +0100

    Update AutoSwitchStatus.plugin.js

--- Plugins/AutoSwitchStatus/AutoSwitchStatus.plugin.js
+++ Plugins/AutoSwitchStatus/AutoSwitchStatus.plugin.js
@@ -1,19 +1,78 @@
 /**
  * @name AutoSwitchStatus
  * @description Automatically switches your discord status to 'away' when you are muted inside a server or 'invisible' when disconnected from a server. For Bugs or Feature Requests open an issue on my Github.
- * @version 1.1.2
+ * @version 1.2.0
  * @author nicola02nb
  * @authorLink https://github.com/nicola02nb
  * @source https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/AutoSwitchStatus
  * @updateUrl https://raw.githubusercontent.com/nicola02nb/BetterDiscord-Stuff/main/Plugins/AutoSwitchStatus/AutoSwitchStatus.plugin.js
  */
+const dropdownStatusOptions = [
+    { label: "Online", value: "online" },
+    { label: "Idle", value: "idle" },
+    { label: "Invisible", value: "invisible" },
+    { label: "Do Not Disturb", value: "dnd" }
+]
 
-const { React, Webpack, Data, DOM } = BdApi;
+const config = {
+    changelog: [
+        {
+            title: "1.2.0",
+            type: "improved",
+            items: [
+                "Updated for new BetterDiscord plugin system",
+            ]
+        }
+    ],
+    settings: [{
+        type: "category",
+        id: "statuseSettings",
+        name: "Status Settings",
+        collapsible: true,
+        shown: true,
+        settings: [
+            {
+                type: "dropdown",
+                id: "mutedMicrophoneStatus",
+                name: "Status for muted Microphone:",
+                value: "online",
+                options: dropdownStatusOptions
+            },
+            {
+                type: "dropdown",
+                id: "mutedSoundStatus",
+                name: "Status for muted Sound:",
+                value: "idle",
+                options: dropdownStatusOptions
+            },
+            {
+                type: "dropdown",
+                id: "connectedStatus",
+                name: "Status for connected:",
+                value: "online",
+                options: dropdownStatusOptions
+            },
+            {
+                type: "dropdown",
+                id: "disconnectedStatus",
+                name: "Status for disconnected:",
+                value: "invisible",
+                options: dropdownStatusOptions
+            },
+        ]
+    },
+    { 
+        type: "switch",
+        id: "showToast",
+        name: "Show Toast",
+        note: "If enabled, displays a toast message when the status changes",
+        value: true
+    }]
+};
+
+const { Webpack, DOM } = BdApi;
 const DiscordModules = Webpack.getModule(m => m.dispatch && m.subscribe);
 const SelectedChannelStore = BdApi.Webpack.getStore("SelectedChannelStore");
-const { FormSwitch, FormItem, FormTitle, FormText, SearchableSelect } = Webpack.getByKeys('FormSwitch', 'FormItem', 'FormTitle', 'Select');
-const { useState } = React;
-const Margins = Webpack.getByKeys('marginBottom20');
 
 const UserSettingsProtoUtils = Webpack.getModule(
     (m) =>
@@ -23,14 +82,11 @@ const UserSettingsProtoUtils = Webpack.getModule(
 );
 
 module.exports = class AutoSwitchStatus {
-    constructor() {
-        this.defaultSettings = {
-            mutedSoundStatus: "idle",
-            mutedMicrophoneStatus: "online",
-            connectedStatus: "online",
-            disconnectedStatus: "invisible",
-            showToast: true,
-        };
+    constructor(meta) {
+        this.meta = meta;
+        this.api = new BdApi(this.meta.name);
+        this.initSettingsValues();
+        
         this.locales = {
             online: "Online",
             idle: "Idle",
@@ -39,16 +95,33 @@ module.exports = class AutoSwitchStatus {
         }
     }
 
-    getSetting(key) {
-        return Data.load("AutoSwitchStatus", key) ?? this.defaultSettings[key];
+    initSettingsValues() {
+        for (const setting of config.settings[0].settings) {
+            setting.value = this.api.Data.load(setting.id) ?? setting.value;
+        }
+        config.settings[1].value = this.api.Data.load("mutedSoundStatus") ?? config.settings[1].value;
     }
 
-    setSetting(key, value) {
-        return Data.save("AutoSwitchStatus", key, value);
+    getSettingsPanel() {
+        return BdApi.UI.buildSettingsPanel({
+            settings: config.settings,
+            onChange: (category, id, value) => {
+                if (id === "mutedSoundStatus") config.settings[0].settings[0].value = value;
+                if (id === "mutedMicrophoneStatus") config.settings[0].settings[1].value = value;
+                if (id === "connectedStatus") config.settings[0].settings[2].value = value;
+                if (id === "disconnectedStatus") config.settings[0].settings[3].value = value;
+                if (id === 'showToast') config.settings[1].value = value;
+                this.api.Data.save(id, value);
+            },
+        });
+    }
+
+    getStatusSetting(id) {
+        return config.settings[0].settings.find(setting => setting.id === id).value;
     }
 
     start() {
-        DOM.addStyle("AutoSwitchStatus", `.bd-toast.toast-online.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='12.5' y='10' width='0' height='0' rx='0' ry='0' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%2323a55a' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
+        this.api.DOM.addStyle(`.bd-toast.toast-online.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='12.5' y='10' width='0' height='0' rx='0' ry='0' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%2323a55a' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
         .bd-toast.toast-idle.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='6.25' y='3.75' width='7.5' height='7.5' rx='3.75' ry='3.75' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%23f0b232' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
         .bd-toast.toast-invisible.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='10' y='7.5' width='5' height='5' rx='2.5' ry='2.5' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%2380848e' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
         .bd-toast.toast-dnd.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='8.75' y='8.75' width='7.5' height='2.5' rx='1.25' ry='1.25' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%23f23f43' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}`);
@@ -58,14 +131,6 @@ module.exports = class AutoSwitchStatus {
         this.isConnected = channelId !== null;
         this.isMicrophoneMuted = containerButtons?.children[0]?.getAttribute("aria-checked") === 'true';
         this.isSoundMuted = containerButtons?.children[1]?.getAttribute("aria-checked") === 'true';
-
-        this.settings = {};
-
-        Object.keys(this.defaultSettings).forEach(key => {
-            Object.defineProperty(this.settings, key, {
-                get: () => this.getSetting(key)
-            });
-        });
 
         this.updateUserStatus();
 
@@ -78,7 +143,7 @@ module.exports = class AutoSwitchStatus {
         DiscordModules.unsubscribe("RTC_CONNECTION_STATE", this.handleConnection);
         this.handleConnection = null;
         this.stopMuteObserver();
-        BdApi.DOM.removeStyle("AutoSwitchStatus");
+        this.api.DOM.removeStyle();
     }
 
     handleConnectionStateChange(event) {
@@ -160,16 +225,16 @@ module.exports = class AutoSwitchStatus {
     getUserCurrentStatus() {
         var currStatus;
         if (!this.isConnected) {
-            currStatus = this.settings.disconnectedStatus;
+            currStatus = this.getStatusSetting("disconnectedStatus");
         }
         else if (this.isSoundMuted) {
-            currStatus = this.settings.mutedSoundStatus;
+            currStatus = this.getStatusSetting("mutedSoundStatus");
         }
         else if (this.isMicrophoneMuted) {
-            currStatus = this.settings.mutedMicrophoneStatus;
+            currStatus = this.getStatusSetting("mutedMicrophoneStatus");
         }
         else {
-            currStatus = this.settings.connectedStatus;
+            currStatus = this.getStatusSetting("connectedStatus");
         }
 
         return currStatus;
@@ -196,80 +261,8 @@ module.exports = class AutoSwitchStatus {
      * @param {{}} [options={}]
      */
     showToast(content, options = {}) {
-        if (this.settings.showToast) {
+        if (config.settings[1].value) {
             BdApi.UI.showToast(content, options);
-        }
-    }
-
-    getSettingsPanel() {
-        return () => {
-            const [mutedSoundStatus, setMutedSoundStatus] = useState(this.getSetting('mutedSoundStatus'));
-            const [mutedMicrophoneStatus, setMutedMicrophoneStatus] = useState(this.getSetting('mutedMicrophoneStatus'));
-            const [connectedStatus, setConnectedStatus] = useState(this.getSetting('connectedStatus'));
-            const [disconnectedStatus, setDisconnectedStatus] = useState(this.getSetting('disconnectedStatus'));
-            const [showToast, setShowToast] = useState(this.getSetting('showToast'));
-            const onSelect = (id, value) => {
-                this.setSetting(id, value);
-                if (id === "mutedSoundStatus") setMutedSoundStatus(value);
-                if (id === "mutedMicrophoneStatus") setMutedMicrophoneStatus(value);
-                if (id === "connectedStatus") setConnectedStatus(value);
-                if (id === "disconnectedStatus") setDisconnectedStatus(value);
-            };
-            const onSwitch = (id, value) => {
-                this.setSetting(id, value);
-                if (id === 'showToast') setShowToast(value);
-            };
-            const options = [
-                { label: "Online", value: "online" },
-                { label: "Idle", value: "idle" },
-                { label: "Invisible", value: "invisible" },
-                { label: "Do Not Disturb", value: "dnd" }
-            ];
-            return React.createElement(
-                "div",
-                {},
-                React.createElement(FormItem, { className: Margins.marginBottom20 },
-                    React.createElement(FormTitle, null, "The status selected will be switched to when MUTED SOUND. default: Idle"),
-                    React.createElement(FormText, {}, ""),
-                    React.createElement(SearchableSelect, {
-                        options: options,
-                        value: mutedSoundStatus,
-                        onChange: (value) => onSelect("mutedSoundStatus", value),
-                    })
-                ),
-                React.createElement(FormItem, { className: Margins.marginBottom20 },
-                    React.createElement(FormTitle, null, "Status for muted Microphone:"),
-                    React.createElement(FormText, {}, "The status selected will be switched to when MUTED MICROPHONE. default: Online"),
-                    React.createElement(SearchableSelect, {
-                        options: options,
-                        value: mutedMicrophoneStatus,
-                        onChange: (value) => onSelect("mutedMicrophoneStatus", value),
-                    })
-                ),
-                React.createElement(FormItem, { className: Margins.marginBottom20 },
-                    React.createElement(FormTitle, null, "Status for connected:"),
-                    React.createElement(FormText, {}, "The status selected will be switched to when CONNECTED to a server. default: Online"),
-                    React.createElement(SearchableSelect, {
-                        options: options,
-                        value: connectedStatus,
-                        onChange: (value) => onSelect("connectedStatus", value),
-                    })
-                ),
-                React.createElement(FormItem, { className: Margins.marginBottom20 },
-                    React.createElement(FormTitle, null, "Status for disconnected:"),
-                    React.createElement(FormText, {}, "The status selected will be switched to when DISCONNECTED from a server. default: Invisible"),
-                    React.createElement(SearchableSelect, {
-                        options: options,
-                        value: disconnectedStatus,
-                        onChange: (value) => onSelect("disconnectedStatus", value),
-                    })
-                ),
-                React.createElement(FormSwitch, {
-                    note: "If enabled it hides the krisp button from bottom-left status menu.",
-                    value: showToast,
-                    onChange: (e) => onSwitch('showToast', e),
-                }, "Show Toast"),
-            );
         }
     }
 };
