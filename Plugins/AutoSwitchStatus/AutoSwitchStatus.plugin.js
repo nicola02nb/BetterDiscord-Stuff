f5ba9b77ecc3be3d0c9749c75d63c6be1d6ec7db
Nicola <61830443+nicola02nb@users.noreply.github.com>
2024-12-27 17:09:44 +0100

    Update AutoSwitchStatus.plugin.js

--- Plugins/AutoSwitchStatus/AutoSwitchStatus.plugin.js
+++ Plugins/AutoSwitchStatus/AutoSwitchStatus.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name AutoSwitchStatus
  * @description Automatically switches your discord status to 'away' when you are muted inside a server or 'invisible' when disconnected from a server. For Bugs or Feature Requests open an issue on my Github.
- * @version 1.3.8
+ * @version 1.4.0
  * @author nicola02nb
  * @authorLink https://github.com/nicola02nb
  * @source https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/AutoSwitchStatus
@@ -146,6 +146,7 @@ module.exports = class AutoSwitchStatus {
     start() {
         this.handleConnection = this.handleConnectionStateChange.bind(this);
         this.handleMute = this.handleMuteStateChange.bind(this);
+
         this.api.DOM.addStyle(`.bd-toast.toast-online.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='12.5' y='10' width='0' height='0' rx='0' ry='0' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%2323a55a' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
         .bd-toast.toast-idle.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='6.25' y='3.75' width='7.5' height='7.5' rx='3.75' ry='3.75' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%23f0b232' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
         .bd-toast.toast-invisible.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='10' y='7.5' width='5' height='5' rx='2.5' ry='2.5' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%2380848e' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
@@ -170,8 +171,6 @@ module.exports = class AutoSwitchStatus {
         DiscordModules.unsubscribe("AUDIO_TOGGLE_SELF_MUTE", this.handleMute);
         DiscordModules.unsubscribe("AUDIO_TOGGLE_SELF_DEAF", this.handleMute);
         DiscordModules.unsubscribe("RTC_CONNECTION_STATE", this.handleConnection);
-        this.handleMute = null;
-        this.handleConnection = null;
         this.api.DOM.removeStyle();
     }
 
@@ -240,24 +239,13 @@ module.exports = class AutoSwitchStatus {
      * Updates the remote status to the param `toStatus`
      * @param {('online'|'idle'|'invisible'|'dnd')} toStatus
      */
-    async updateStatus(toStatus) {
-        try {
-            const result = await new Promise((resolve, reject) => {
-                UserSettingsProtoUtils.updateAsync(
-                    "status",
-                    (statusSetting) => {
-                        statusSetting.status.value = toStatus;
-                        resolve(statusSetting); // Resolve when update completes
-                    },
-                    0
-                );
-            });
-            this.showToast(this.locales[toStatus], { type: toStatus });
-            return result;
-        } catch (error) {
-            console.error('Failed to update status:', error);
-            throw error;
-        }
+    updateStatus(toStatus) {
+        UserSettingsProtoUtils.updateAsync(
+            "status",
+            (settings) => {
+                settings.status.value = toStatus;
+            }, 15); // 15 is the seconds after which the status will be updated through the API (Prevents rate limiting)
+        this.showToast(this.locales[toStatus], { type: toStatus });
     }
 
     /**
