b93dd63c6341d792353f75a4cb274279043c8916
Nicola <61830443+nicola02nb@users.noreply.github.com>
2024-12-31 16:03:31 +0100

    Added files for NotifyWhenMuted

---
+++ Plugins/NotifyWhenMuted/NotifyWhenMuted.plugin.js
@@ -0,0 +1,86 @@
+/**
+ * @name NotifyWhenMuted
+ * @description Plays a sound when user tries to speak while muted
+ * @version 1.0.0
+ * @author nicola02nb
+ * @source https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/NotifyWhenMuted
+ * @updateUrl https://raw.githubusercontent.com/nicola02nb/BetterDiscord-Stuff/main/Plugins/NotifyWhenMuted/NotifyWhenMuted.plugin.js
+*/
+
+const defaultAudioUrl = "https://raw.githubusercontent.com/nicola02nb/BetterDiscord-Stuff/main/Plugins/NotifyWhenMuted/stop_talking.wav";
+const config = {
+    changelog: [],
+    settings: [
+        { type: "text", id: "audioUrl", name: "Custom Audio URL", note: "URL to the audio file to play when user tries to speak while muted", value: defaultAudioUrl },
+    ]
+};
+
+const { Webpack, Patcher, React } = BdApi;
+const DiscordModules = Webpack.getModule(m => m.dispatch && m.subscribe);
+const getConnectedUser = Webpack.getByKeys("getCurrentUser");
+const userProfileMod = Webpack.getByKeys("getUserProfile");
+
+var console = {};
+
+
+module.exports = class NotifyWhenMuted {
+    constructor(meta) {
+        this.meta = meta;
+        this.api = new BdApi(this.meta.name);
+        console = this.api.Logger;
+        this.initSettingsValues();
+    }
+
+    initSettingsValues() {
+        config.settings[0].value = this.api.Data.load("audioUrl") ?? config.settings[0].value;
+    }
+
+    getSettingsPanel() {
+        return BdApi.UI.buildSettingsPanel({
+            settings: config.settings,
+            onChange: (category, id, value) => {
+                switch (id) {
+                    case "audioUrl":
+                        value = this.isValidURL(value);
+                        config.settings[0].value = value;
+                    break;
+                }
+                this.api.Data.save(id, value);
+            }
+        });
+    }
+
+    isValidURL(string) {
+        try {
+            new URL(string);
+            console.log("Valid URL");
+            return string;
+        } catch (e) {
+            console.error("Invalid URL");
+            return defaultAudioUrl;
+        }
+    }
+
+    start() {
+        this.handleSpeak = this.handleSpeaking.bind(this);
+
+        this.playing = false;
+        this.userId = getConnectedUser.getCurrentUser().id;
+
+        DiscordModules.subscribe("SPEAKING", this.handleSpeak);
+    }
+
+    stop() {
+        DiscordModules.unsubscribe("SPEAKING", this.handleSpeak);
+        Patcher.unpatchAll(this.meta.name);
+    }
+
+    handleSpeaking(event) {
+        if (!this.playing && event.userId === this.userId) {
+            this.playing = true;
+            const audio = new Audio(config.settings[0].value);
+            audio.onended = () => this.playing = false;
+            audio.play();
+        }
+    }
+};
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A bunch of stuff for Betterdiscord by Me 🙃
 ## Plugins
 - [AutoSwitchStatus](https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/AutoSwitchStatus)
 - [BetterTTS](https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/BetterTTS)
+- [NotifyWhenMuted](https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/NotifyWhenMuted)
 - [ShowPing](https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/ShowPing)
