cac1afdaa10e1150b8b9ec23a71b54bfe8fa8fcc
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-01-02 13:32:13 +0100

    Update NotifyWhenMuted.plugin.js

--- Plugins/NotifyWhenMuted/NotifyWhenMuted.plugin.js
+++ Plugins/NotifyWhenMuted/NotifyWhenMuted.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name NotifyWhenMuted
  * @description Plays a sound when user tries to speak while muted
- * @version 1.0.0
+ * @version 1.1.0
  * @author nicola02nb
  * @source https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/NotifyWhenMuted
  * @updateUrl https://raw.githubusercontent.com/nicola02nb/BetterDiscord-Stuff/main/Plugins/NotifyWhenMuted/NotifyWhenMuted.plugin.js
@@ -12,6 +12,7 @@ const config = {
     changelog: [],
     settings: [
         { type: "text", id: "audioUrl", name: "Custom Audio URL", note: "URL to the audio file to play when user tries to speak while muted", value: defaultAudioUrl },
+        { type: "number", id: "delayBetweenNotifications", name: "Delay Between Audio Notifictions (ms)", note: "Delay Between Audio Notifictions in milliseconds", value: 5000 },
     ]
 };
 
@@ -31,6 +32,7 @@ module.exports = class NotifyWhenMuted {
 
     initSettingsValues() {
         config.settings[0].value = this.api.Data.load("audioUrl") ?? config.settings[0].value;
+        config.settings[1].value = this.api.Data.load("delayBetweenNotifications") ?? config.settings[1].value;
     }
 
     getSettingsPanel() {
@@ -41,7 +43,11 @@ module.exports = class NotifyWhenMuted {
                     case "audioUrl":
                         value = this.isValidURL(value);
                         config.settings[0].value = value;
-                    break;
+                        break;
+                    case "delayBetweenNotifications":
+                        value = parseInt(value);
+                        config.settings[1].value = value;
+                        break;
                 }
                 this.api.Data.save(id, value);
             }
@@ -60,27 +66,28 @@ module.exports = class NotifyWhenMuted {
     start() {
         this.handleSpeak = this.handleSpeaking.bind(this);
 
-        this.audio = null;
+        this.isPlaying = false;
 
         Patcher.after(this.meta.name, MediaEngineStore, "getSpeakingWhileMuted", this.handleSpeak);
     }
 
     stop() {
         Patcher.unpatchAll(this.meta.name);
-        if(this.audio) {
+        if (this.audio) {
             this.audio.pause();
             this.audio = null;
         }
     }
 
-    handleSpeaking(_, args, ret) {
-        if (ret) {
-            if (this.audio && !this.audio.ended) {
-                return; // Prevent multiple audio from playing together
-            }
+    async handleSpeaking(_, args, ret) {
+        const delay = ms => new Promise(res => setTimeout(res, ms));
+        if (ret && !this.isPlaying) {
+            console.log(this);
+            this.isPlaying = true;
             this.audio = new Audio(config.settings[0].value);
-            this.audio.onended = () => this.audio = null;
-            this.audio.play();  
+            this.audio.play();
+            await delay(config.settings[1].value);
+            this.isPlaying = false;
         }
     }
 };
