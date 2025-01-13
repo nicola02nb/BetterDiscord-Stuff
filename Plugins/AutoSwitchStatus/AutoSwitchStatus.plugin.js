1fefed2ca10a5d4881e97198c78f280e11d10c72
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-01-13 10:59:18 +0100

    Update AutoSwitchStatus.plugin.js

--- Plugins/AutoSwitchStatus/AutoSwitchStatus.plugin.js
+++ Plugins/AutoSwitchStatus/AutoSwitchStatus.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name AutoSwitchStatus
  * @description Automatically switches your discord status to 'away' when you are muted inside a server or 'invisible' when disconnected from a server. For Bugs or Feature Requests open an issue on my Github.
- * @version 1.6.0
+ * @version 1.7.0
  * @author nicola02nb
  * @authorLink https://github.com/nicola02nb
  * @source https://github.com/nicola02nb/BetterDiscord-Stuff/tree/main/Plugins/AutoSwitchStatus
@@ -12,54 +12,26 @@ const dropdownStatusOptions = [
     { label: "Idle", value: "idle" },
     { label: "Invisible", value: "invisible" },
     { label: "Do Not Disturb", value: "dnd" }
-]
+];
 
 const config = {
     changelog: [],
-    settings: [{
-        type: "category",
-        id: "statuseSettings",
-        name: "Status Settings",
-        collapsible: true,
-        shown: true,
-        settings: [
-            {
-                type: "dropdown",
-                id: "mutedMicrophoneStatus",
-                name: "Status for muted Microphone:",
-                value: "online",
-                options: dropdownStatusOptions
-            },
-            {
-                type: "dropdown",
-                id: "mutedSoundStatus",
-                name: "Status for muted Sound:",
-                value: "idle",
-                options: dropdownStatusOptions
-            },
-            {
-                type: "dropdown",
-                id: "connectedStatus",
-                name: "Status for connected:",
-                value: "online",
-                options: dropdownStatusOptions
-            },
-            {
-                type: "dropdown",
-                id: "disconnectedStatus",
-                name: "Status for disconnected:",
-                value: "invisible",
-                options: dropdownStatusOptions
-            },
-        ]
-    },
-    {
-        type: "switch",
-        id: "showToast",
-        name: "Show Toast",
-        note: "If enabled, displays a toast message when the status changes",
-        value: true
-    }]
+    settings: [
+        {
+            type: "category",
+            id: "statuseSettings",
+            name: "Status Settings",
+            collapsible: true,
+            shown: true,
+            settings: [
+                { type: "dropdown", id: "mutedMicrophoneStatus", name: "Status for muted Microphone:", value: "online", options: dropdownStatusOptions },
+                { type: "dropdown", id: "mutedSoundStatus", name: "Status for muted Sound:", value: "idle", options: dropdownStatusOptions },
+                { type: "dropdown", id: "connectedStatus", name: "Status for connected:", value: "online", options: dropdownStatusOptions },
+                { type: "dropdown", id: "disconnectedStatus", name: "Status for disconnected:", value: "invisible", options: dropdownStatusOptions }
+            ]
+        },
+        { type: "switch", id: "showToast", name: "Show Toast", note: "If enabled, displays a toast message when the status changes", value: true }
+    ]
 };
 
 function setConfigSetting(id, newValue) {
@@ -77,40 +49,13 @@ function setConfigSetting(id, newValue) {
             }
         }
     }
-
-}
-
-function getConfigSetting(id) {
-    for (const setting of config.settings) {
-        if (setting.id === id) {
-            return setting.value;
-        }
-        if (setting.settings) {
-            for (const settingInt of setting.settings) {
-                if (settingInt.id === id) {
-                    return settingInt.value;
-                }
-            }
-        }
-    }
-}
-
-function initSettingsValues() {
-    for (const setting of config.settings) {
-        if (setting.type === "category") {
-            for (const settingInt of setting.settings) {
-                settingInt.value = Data.load("AutoSwitchStatus", settingInt.id) ?? settingInt.value;
-            }
-        } else {
-            setting.value = Data.load("AutoSwitchStatus", setting.id) ?? setting.value;
-        }
-    }
 }
 
 const { Webpack, Data } = BdApi;
 const DiscordModules = Webpack.getModule(m => m.dispatch && m.subscribe);
 const SelectedChannelStore = Webpack.getStore("SelectedChannelStore");
 const MediaEngineStore = Webpack.getStore("MediaEngineStore");
+
 var console = {};
 
 const UserSettingsProtoUtils = Webpack.getModule(
@@ -125,8 +70,8 @@ module.exports = class AutoSwitchStatus {
         this.meta = meta;
         this.api = new BdApi(this.meta.name);
         console = this.api.Logger;
-        initSettingsValues();
 
+        this.settings = {};
         this.locales = {
             online: "Online",
             idle: "Idle",
@@ -135,10 +80,25 @@ module.exports = class AutoSwitchStatus {
         }
     }
 
+    initSettingsValues() {
+        for (const setting of config.settings) {
+            if (setting.type === "category") {
+                for (const settingInt of setting.settings) {
+                    settingInt.value = Data.load("BetterTTS", settingInt.id) ?? settingInt.value;
+                    this.settings[settingInt.id] = settingInt.value;
+                }
+            } else {
+                setting.value = Data.load("BetterTTS", setting.id) ?? setting.value;
+                this.settings[setting.id] = setting.value;
+            }
+        }
+    }
+
     getSettingsPanel() {
         return BdApi.UI.buildSettingsPanel({
             settings: config.settings,
             onChange: (category, id, value) => {
+                this.settings[id] = value;
                 setConfigSetting(id, value);
             },
         });
@@ -147,7 +107,8 @@ module.exports = class AutoSwitchStatus {
     start() {
         this.handleConnection = this.handleConnectionStateChange.bind(this);
         this.handleMute = this.handleMuteStateChange.bind(this);
-
+        
+        this.initSettingsValues();
         this.api.DOM.addStyle(`.bd-toast.toast-online.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='12.5' y='10' width='0' height='0' rx='0' ry='0' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%2323a55a' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
         .bd-toast.toast-idle.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='6.25' y='3.75' width='7.5' height='7.5' rx='3.75' ry='3.75' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%23f0b232' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
         .bd-toast.toast-invisible.icon {background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' %3E%3Cmask id=':r1d:'%3E%3Crect x='7.5' y='5' width='10' height='10' rx='5' ry='5' fill='white'%3E%3C/rect%3E%3Crect x='10' y='7.5' width='5' height='5' rx='2.5' ry='2.5' fill='black'%3E%3C/rect%3E%3Cpolygon points='-2.16506,-2.5 2.16506,0 -2.16506,2.5' fill='black' transform='scale(0) translate(13.125 10)' style='transform-origin: 13.125px 10px;'%3E%3C/polygon%3E%3Ccircle fill='black' cx='12.5' cy='10' r='0'%3E%3C/circle%3E%3C/mask%3E%3Crect fill='%2380848e' width='25' height='15' mask='url(%23:r1d:)'%3E%3C/rect%3E%3C/svg%3E");}
@@ -215,16 +176,16 @@ module.exports = class AutoSwitchStatus {
     getUserCurrentStatus() {
         var currStatus;
         if (!this.isConnected) {
-            currStatus = getConfigSetting("disconnectedStatus");
+            currStatus = this.settings.disconnectedStatus;
         }
         else if (this.isSoundMuted) {
-            currStatus = getConfigSetting("mutedSoundStatus");
+            currStatus = this.settings.mutedSoundStatus;
         }
         else if (this.isMicrophoneMuted) {
-            currStatus = getConfigSetting("mutedMicrophoneStatus");
+            currStatus = this.settings.mutedMicrophoneStatus;
         }
         else {
-            currStatus = getConfigSetting("connectedStatus");
+            currStatus = this.settings.connectedStatus;
         }
 
         return currStatus;
@@ -249,7 +210,7 @@ module.exports = class AutoSwitchStatus {
      * @param {{}} [options={}]
      */
     showToast(content, options = {}) {
-        if (getConfigSetting("showToast")) {
+        if (this.settings.showToast) {
             BdApi.UI.showToast(content, options);
         }
     }
