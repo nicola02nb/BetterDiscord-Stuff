09f58d399d4ee48d64fa7ae40ebd77a1410858d2
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-07-21 18:53:55 +0200

    Update ShowPing.plugin.js

--- Plugins/ShowPing/ShowPing.plugin.js
+++ Plugins/ShowPing/ShowPing.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name ShowPing
- * @description Displays your live ping. For Bugs or Feature Requests open an issue on my Github.
- * @version 2.5.5
+ * @description Displays your live ping
+ * @version 2.6.0
  * @author nicola02nb
  * @invite hFuY8DfDGK
  * @authorLink https://github.com/nicola02nb
@@ -20,32 +20,29 @@ const config = {
     ]
 };
 
-const { Webpack, Patcher } = BdApi;
+const { Webpack, React, Data, DOM, Patcher } = BdApi;
 const DiscordModules = Webpack.getModule(m => m.dispatch && m.subscribe);
 const RTCConnectionStore = Webpack.getStore("RTCConnectionStore");
 
-const { labelWrapper, rtcConnectionStatus, rtcConnectionStatusConnected } = Webpack.getByKeys("labelWrapper", "rtcConnectionStatus", "rtcConnectionStatusConnected");
+const { labelWrapper, rtcConnectionStatusConnected } = Webpack.getByKeys("labelWrapper", "rtcConnectionStatusConnected");
 const { voiceButtonsContainer } = Webpack.getByKeys("voiceButtonsContainer");
 const labelClasses = Webpack.getModule(m => m["hovered"] && m["default"]);
-const textMdMedium = BdApi.Webpack.getModule(m => m["text-md/medium"] && !m["avatar"] && !m["defaultColor"])["text-md/medium"];
-const ConnectionStatus = Webpack.getModule(Webpack.Filters.byStrings("rtcConnectionStatusWrapper")).prototype;
-
-var console = {};
-
+const textMdMedium = Webpack.getModule(m => m["text-md/medium"] && !m["avatar"] && !m["defaultColor"])["text-md/medium"];
+const ConnectionStatus = Webpack.getModule((exports, module, id) => id === "423516");
 module.exports = class ShowPing {
     constructor(meta) {
         this.meta = meta;
-        this.BdApi = new BdApi(this.meta.name);
-        console = this.BdApi.Logger;
         this.initSettingsValues();
 
         this.statusBar = null;
         this.pingElement = null;
         this.pingObserver = null;
+
+        this.updatePing = null;
     }
 
     initSettingsValues() {
-        config.settings[0].value = this.BdApi.Data.load("hideKrispButton") ?? config.settings[0].value;
+        config.settings[0].value = Data.load(this.meta.name, "hideKrispButton") ?? config.settings[0].value;
     }
 
     getSettingsPanel() {
@@ -56,97 +53,57 @@ module.exports = class ShowPing {
                     config.settings[0].value = value;
                     this.displayKrispButton(!value);
                 }
-                this.BdApi.Data.save(id, value);
+                Data.save(this.meta.name, id, value);
             },
         });
     }
 
     start() {
-        this.handleConnection = this.handleConnectionStateChange.bind(this);
-        this.handlePing = this.handlePingChange.bind(this);
-        this.BdApi.DOM.addStyle(`.${rtcConnectionStatusConnected.split(" ")[0]}{display: flex;}
-            .${voiceButtonsContainer}{margin-left: 2px !important;}
+        DOM.addStyle(this.meta.name, `
+            .${rtcConnectionStatusConnected.split(" ")[0]} .${labelClasses["default"]} {display: flex; !important; }
+            .${voiceButtonsContainer} {margin-left: 2px !important;}
             .${labelWrapper} > button {width: 100%; display: inline;}
-            .${labelClasses["hovered"]} > div {text-overflow: ellipsis; overflow: hidden;}
             .${labelClasses["default"]} > div {text-overflow: ellipsis; overflow: hidden;}
-            .pingDisplay{min-width: min-content;}`);
-        this.addPingDisplay();
-        DiscordModules.subscribe("RTC_CONNECTION_STATE", this.handleConnection);
-        DiscordModules.subscribe("RTC_CONNECTION_PING", this.handlePing);
-        Patcher.after(this.meta.name, ConnectionStatus, "renderStatus", (_, [props], ret) => {
-            if (!this.pingElement) {
-                this.addPingDisplay();
+            .${labelClasses["hover"]} > div {text-overflow: ellipsis; overflow: hidden;}
+            .pingDisplay {min-width: min-content;}`);
+        
+        Patcher.after(this.meta.name, ConnectionStatus, "Z", (_, args, ret) => {
+            const container = ret;
+            container.props.children = [container.props.children];
+            if (Array.isArray(container?.props?.children)) {
+                container?.props?.children?.push(React.createElement(this.PingElement));
             }
+            return ret;
         });
     }
 
     stop() {
         Patcher.unpatchAll(this.meta.name);
         DiscordModules.unsubscribe("RTC_CONNECTION_PING", this.handlePing);
-        DiscordModules.unsubscribe("RTC_CONNECTION_STATE", this.handleConnection);
-        this.removePingDisplay();
         this.displayKrispButton(true);
-        this.BdApi.DOM.removeStyle();
+        DOM.removeStyle(this.meta.name);
     }
 
-    handleConnectionStateChange(event) {
-        if (event.context === "default") {
-            if (event.state === "RTC_CONNECTED") {
-                this.addPingDisplay();
-            } else {
-                this.removePingDisplay();
-            }
-        }
-    }
+    PingElement() {
+        const [ping, setPing] = React.useState(RTCConnectionStore.getLastPing());
 
-    handlePingChange(event) {
-        if (this.pingElement) {
-            this.updatePing();
-        }
-    }
+        const updatePing = (_) => {
+            setPing(RTCConnectionStore.getLastPing());
+        };
 
-    addPingDisplay() {
-        const connection = document.getElementsByClassName(rtcConnectionStatus)[0];
-        this.statusBar = connection?.getElementsByClassName(rtcConnectionStatusConnected)[0];
+        DiscordModules.subscribe("RTC_CONNECTION_PING", updatePing);
 
-        if (this.statusBar) {
-            // Create ping display element
-            this.pingElement = document.createElement('div');
-            this.pingElement.className = `${textMdMedium} pingDisplay`;
-            // Insert ping element after the status text
-            this.statusBar.appendChild(this.pingElement);
-
-            this.updatePing();
-            if (config.settings[0].value) {
-                this.displayKrispButton(false);
-            }
-        }
-    }
-
-    removePingDisplay() {
-        if (this.pingElement) {
-            this.pingElement.remove();
-            this.pingElement = null;
-        }
-        if (this.statusBar) {
-            this.statusBar = null;
-        }
-    }
-
-    updatePing() {
-        let ping = RTCConnectionStore.getLastPing();
-        if (ping === null || ping === undefined || ping === "") {
-            this.pingElement.textContent = `\u00A0N/A`;
-        } else if (this.pingElement && this.pingElement.isConnected) {
-            this.pingElement.textContent = `\u00A0${ping} ms`;
-        }
+        return React.createElement("div", {
+            className: `${textMdMedium} pingDisplay`,
+            children: "\u00A0" + (ping !== undefined ? `${ping} ms` : "N/A")
+        });
     }
 
     displayKrispButton(show) {
         if (show) {
-            this.BdApi.DOM.removeStyle("hidekrispStyle");
+            DOM.removeStyle(this.meta.name+"-hidekrispStyle");
         } else {
-            this.BdApi.DOM.addStyle("hidekrispStyle", `.${voiceButtonsContainer} > button[aria-label*="Krisp"] {display: none !important;}`);
+            DOM.addStyle(this.meta.name+"-hidekrispStyle", `.${voiceButtonsContainer} > button[aria-label*="Krisp"] {display: none !important;}`);
         }
     }
 };
