689fd824ee80fc1ce2615e58ade2b09be0cc2fc6
Nicola <61830443+nicola02nb@users.noreply.github.com>
2026-02-09 15:39:42 +0100

    Fiex plugin not starting

--- Plugins/NotifyWhenMuted/NotifyWhenMuted.plugin.js
+++ Plugins/NotifyWhenMuted/NotifyWhenMuted.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name NotifyWhenMuted
  * @description Plays a sound when user tries to speak while muted
- * @version 1.4.17
+ * @version 1.4.18
  * @author nicola02nb
  * @invite hFuY8DfDGK
  * @authorLink https://github.com/nicola02nb
@@ -33,12 +33,13 @@ function getSetting(key) {
 const { Webpack, Patcher, React, Components, Data, DOM, UI } = BdApi;
 const { Filters } = Webpack;
 
-const [MediaEngineStore, buttonStates, buttonLook, voiceButtonsContainer] = Webpack.getBulk(
+const [MediaEngineStore, buttonStates, buttonLook] = Webpack.getBulk(
     { filter: Filters.byStoreName("MediaEngineStore") },
     { filter: Filters.byKeys("enabled", "button") },
-    { filter: Filters.byKeys("button", "lookBlank", "colorBrand", "grow") },
-    { filter: Filters.bySource("wrap:", "=E.NO_WRAP", "shrink") }
+    { filter: Filters.byKeys("button", "lookBlank", "colorBrand", "grow") }
 );
+
+const voiceButtonsContainer = [...Webpack.getWithKey(Filters.byStrings("NO_WRAP", "flexShrink:"), { searchExports: true })];
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
@@ -134,7 +135,7 @@ module.exports = class NotifyWhenMuted {
         this.isPlaying = false;
 
         Patcher.after(this.meta.name, MediaEngineStore, "getSpeakingWhileMuted", this.handleSpeak);
-        Patcher.after(this.meta.name, voiceButtonsContainer, "A", this.addButton);
+        Patcher.after(this.meta.name, voiceButtonsContainer[0], voiceButtonsContainer[1], this.addButton);
     }
 
     stop() {
@@ -164,58 +165,60 @@ module.exports = class NotifyWhenMuted {
 
     handleAddButton(_, args, ret) {
         if (!this.settings.showToggleButton) return ret;
-        const ToggleButton = () => {
-            const [enabled, setEnabled] = React.useState(this.settings.enabled);
-            const update = () => {
-                this.updateSetting("enabled", !enabled);
-                setEnabled(!enabled);
-            };
-
-            const getIcon = (enabled) => {
-                return React.createElement("svg", {
-                    xmlns: "http://www.w3.org/2000/svg",
-                    width: "20",
-                    height: "20",
-                    fill: "currentColor",
-                    viewBox: "0 0 16 16"
-                },
-                    React.createElement("path", {
-                        d: "M 8 16 a 2 2 0 0 0 2 -2 l -4 0 a 2 2 0 0 0 2 2 m 0 -14.1 l -0.8 0.2 a 4 4 0 0 0 -3.2 3.9 c 0 0.6 -0.1 2.2 -0.5 3.7 c -0.2 0.8 -0.4 1.6 -0.7 2.3 l 10.2 0 c -0.3 -0.7 -0.5 -1.5 -0.7 -2.3 c -0.3 -1.5 -0.5 -3.1 -0.5 -3.7 a 4 4 0 0 0 -3.2 -3.9 l -0.8 -0.2 z m 6.2 10.1 c 0.2 0.5 0.5 0.8 0.8 1 l -14 0 c 0.3 -0.2 0.6 -0.6 0.8 -1 c 0.9 -1.8 1.2 -5.1 1.2 -6 c 0 -2.4 1.7 -4.4 4 -4.9 a 1 1 0 1 1 2 0 a 5 5 0 0 1 4 4.9 c 0 0.9 0.3 4.2 1.2 6",
-                        fill: !enabled ? "var(--status-danger)" : "currentColor"
-                    }
-                    ),
-                    React.createElement("g", { transform: "translate(4.7,3.3) scale(1,1) translate(-4.7,-3.3) translate(4.7,11.4) matrix(0.5,0,0,0.5,-0.8,-8)" },
-                        React.createElement("path", {
-                            d: "M 13 8 c 0 0.6 -0.1 1.1 -0.3 1.6 l -0.8 -0.8 a 4 4 0 0 0 0.1 -0.8 l 0 -1 a 0.5 0.5 0 0 1 1 0 l 0 1 z m -5 4 c 0.8 0 1.6 -0.2 2.2 -0.7 l 0.7 0.7 a 5 5 0 0 1 -2.4 0.9 l 0 2 l 3 0 a 0.5 0.5 0 0 1 0 1 l -7 0 a 0.5 0.5 0 0 1 0 -1 l 3 0 l 0 -2 a 5 5 0 0 1 -4.5 -5 l 0 -1 a 0.5 0.5 0 0 1 1 0 l 0 1 a 4 4 0 0 0 4 4 m 3 -9 l 0 4.9 l -5.8 -5.8 a 3 3 0 0 1 5.8 1",
-                            fill: "currentColor"
-                        }
-                        ),
-                        React.createElement("path", {
-                            d: "M 9.5 10.6 l -4.5 -4.5 l 0 1.9 a 3 3 0 0 0 4.5 2.6 m -7.8 -9.3 l 12 12 l 0.7 -0.7 l -12 -12 l -0.7 0.7 z",
-                            fill: "currentColor"
-                        }
-                        )
-                    )
-                );
-            }
-
-            return React.createElement(Components.Tooltip, {
-                text: enabled ? "Notify Muted Enabled" : "Notify Muted Disabled"
-            }, (props) => React.createElement(
-                "button", {
-                ...props,
-                className: "toggleNotifyMuted " + buttonStates.button + " " + buttonStates.enabled + " " + buttonLook.button + " " + buttonLook.lookBlank + " " + buttonLook.colorBrand + " " + buttonLook.grow,
-                onClick: () => {
-                    update();
-                },
-            }, getIcon(enabled)
-            ))
-        };
+        
         if (args[0]?.className?.includes("voiceButtonsContainer_")) {
-            const button = React.createElement(ToggleButton);
-            button.type = ToggleButton;
+            const button = React.createElement(this.ToggleButton);
+            //button.type = ToggleButton;
             ret.props?.children?.unshift(button);
         }
         return ret;
+    }
+
+    ToggleButton = () => {
+        const [enabled, setEnabled] = React.useState(this.settings.enabled);
+        const update = () => {
+            this.updateSetting("enabled", !enabled);
+            setEnabled(!enabled);
+        };
+
+        const getIcon = (enabled) => {
+            return React.createElement("svg", {
+                xmlns: "http://www.w3.org/2000/svg",
+                width: "20",
+                height: "20",
+                fill: "currentColor",
+                viewBox: "0 0 16 16"
+            },
+                React.createElement("path", {
+                    d: "M 8 16 a 2 2 0 0 0 2 -2 l -4 0 a 2 2 0 0 0 2 2 m 0 -14.1 l -0.8 0.2 a 4 4 0 0 0 -3.2 3.9 c 0 0.6 -0.1 2.2 -0.5 3.7 c -0.2 0.8 -0.4 1.6 -0.7 2.3 l 10.2 0 c -0.3 -0.7 -0.5 -1.5 -0.7 -2.3 c -0.3 -1.5 -0.5 -3.1 -0.5 -3.7 a 4 4 0 0 0 -3.2 -3.9 l -0.8 -0.2 z m 6.2 10.1 c 0.2 0.5 0.5 0.8 0.8 1 l -14 0 c 0.3 -0.2 0.6 -0.6 0.8 -1 c 0.9 -1.8 1.2 -5.1 1.2 -6 c 0 -2.4 1.7 -4.4 4 -4.9 a 1 1 0 1 1 2 0 a 5 5 0 0 1 4 4.9 c 0 0.9 0.3 4.2 1.2 6",
+                    fill: !enabled ? "var(--status-danger)" : "currentColor"
+                }
+                ),
+                React.createElement("g", { transform: "translate(4.7,3.3) scale(1,1) translate(-4.7,-3.3) translate(4.7,11.4) matrix(0.5,0,0,0.5,-0.8,-8)" },
+                    React.createElement("path", {
+                        d: "M 13 8 c 0 0.6 -0.1 1.1 -0.3 1.6 l -0.8 -0.8 a 4 4 0 0 0 0.1 -0.8 l 0 -1 a 0.5 0.5 0 0 1 1 0 l 0 1 z m -5 4 c 0.8 0 1.6 -0.2 2.2 -0.7 l 0.7 0.7 a 5 5 0 0 1 -2.4 0.9 l 0 2 l 3 0 a 0.5 0.5 0 0 1 0 1 l -7 0 a 0.5 0.5 0 0 1 0 -1 l 3 0 l 0 -2 a 5 5 0 0 1 -4.5 -5 l 0 -1 a 0.5 0.5 0 0 1 1 0 l 0 1 a 4 4 0 0 0 4 4 m 3 -9 l 0 4.9 l -5.8 -5.8 a 3 3 0 0 1 5.8 1",
+                        fill: "currentColor"
+                    }
+                    ),
+                    React.createElement("path", {
+                        d: "M 9.5 10.6 l -4.5 -4.5 l 0 1.9 a 3 3 0 0 0 4.5 2.6 m -7.8 -9.3 l 12 12 l 0.7 -0.7 l -12 -12 l -0.7 0.7 z",
+                        fill: "currentColor"
+                    }
+                    )
+                )
+            );
+        }
+
+        return React.createElement(Components.Tooltip, {
+            text: enabled ? "Notify Muted Enabled" : "Notify Muted Disabled"
+        }, (props) => React.createElement(
+            "button", {
+            ...props,
+            className: "toggleNotifyMuted " + buttonStates.button + " " + buttonStates.enabled + " " + buttonLook.button + " " + buttonLook.lookBlank + " " + buttonLook.colorBrand + " " + buttonLook.grow,
+            onClick: () => {
+                update();
+            },
+        }, getIcon(enabled)
+        ))
     }
 };
